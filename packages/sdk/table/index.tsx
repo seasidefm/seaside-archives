f1f09f995d251b342577879b557bc5a1be36cf68
Doug Flynn <DouglassHooker@gmail.com>
2022-07-06 19:42:33 +0000

    Add color to turbo, run prettier

--- packages/sdk/table/index.tsx
+++ packages/sdk/table/index.tsx
@@ -3,5 +3,5 @@ import { DataGrid, DataGridProps } from "@mui/x-data-grid";
 type SeasideTableProps = DataGridProps;
 
 export const SeasideTable: React.FC<SeasideTableProps> = (props) => {
-  return <DataGrid {...props} />;
+    return <DataGrid {...props} />;
 };
