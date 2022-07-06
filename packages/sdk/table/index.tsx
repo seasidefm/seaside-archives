2217fdc5dc8d1b41b8e58fe314d2b8319b071644
Doug Flynn <DouglassHooker@gmail.com>
2022-07-06 19:33:15 +0000

    Remove example code. Add real projects

---
+++ packages/sdk/table/index.tsx
@@ -0,0 +1,7 @@
+import { DataGrid, DataGridProps } from "@mui/x-data-grid";
+
+type SeasideTableProps = DataGridProps;
+
+export const SeasideTable: React.FC<SeasideTableProps> = (props) => {
+  return <DataGrid {...props} />;
+};
