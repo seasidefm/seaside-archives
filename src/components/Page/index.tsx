f43c37cbeecf24c7aaa646b5a7d10030df9428b3
Doug Flynn <DouglassHooker@gmail.com>
2022-08-03 01:34:28 +0000

    Add react query

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 import styles from "../../../styles/Home.module.css";
 import { Navbar } from "@components/Navbar";
+
+const queryClient = new QueryClient();
 
 export const Page = ({ children }: React.PropsWithChildren) => {
     const [darkMode, setDarkMode] = useState(false);
@@ -23,45 +26,47 @@ export const Page = ({ children }: React.PropsWithChildren) => {
     });
 
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Box
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    height: "100vh",
-                }}
-            >
-                <Head>
-                    <title>SeasideFM Archives</title>
-                    <meta
-                        name="description"
-                        content="Favorites and VODs for SeasideFM"
-                    />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-
-                <Navbar />
-
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
                 <Box
-                    component={"main"}
                     sx={{
-                        flex: 1,
+                        display: "flex",
+                        flexDirection: "column",
+                        height: "100vh",
                     }}
                 >
-                    {children}
-                </Box>
+                    <Head>
+                        <title>SeasideFM Archives</title>
+                        <meta
+                            name="description"
+                            content="Favorites and VODs for SeasideFM"
+                        />
+                        <link rel="icon" href="/favicon.ico" />
+                    </Head>
 
-                <footer className={styles.footer}>
-                    <a
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                    <Navbar />
+
+                    <Box
+                        component={"main"}
+                        sx={{
+                            flex: 1,
+                        }}
                     >
-                        Copyright 2022 SeasideFM
-                    </a>
-                </footer>
-            </Box>
-        </ThemeProvider>
+                        {children}
+                    </Box>
+
+                    <footer className={styles.footer}>
+                        <a
+                            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Copyright 2022 SeasideFM
+                        </a>
+                    </footer>
+                </Box>
+            </ThemeProvider>
+        </QueryClientProvider>
     );
 };
