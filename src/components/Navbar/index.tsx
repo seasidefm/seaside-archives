0bf1664e5b9a870dce5f8366b8dea11b75cd5324
Doug Flynn <DouglassHooker@gmail.com>
2022-08-06 21:22:17 -0400

    Add twitch redirect

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,14 @@
 import { AppBar, Toolbar } from "@mui/material";
 import React from "react";
 
+const TWITCH_REDIRECT = process.env.TWITCH_REDIRECT_URI;
+
 export const Navbar: React.FC = () => {
-  return (
-    <AppBar variant="elevation">
-      <Toolbar />
-    </AppBar>
-  );
+    return (
+        <AppBar variant="elevation">
+            <Toolbar>
+                <a href={TWITCH_REDIRECT}>Login with Twitch</a>
+            </Toolbar>
+        </AppBar>
+    );
 };
